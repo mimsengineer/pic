18884127ed382aeedb9fdeaf255476756846ee40
Hafsa ASSILA <101941769+C-team-project@users.noreply.github.com>
2025-01-01 12:34:00 +0100

    Update MainHeader.tsx

--- Netflix-clone/src/components/layouts/MainHeader.tsx
+++ Netflix-clone/src/components/layouts/MainHeader.tsx
@@ -16,7 +16,7 @@ import Logo from "../Logo";
 import SearchBox from "../SearchBox";
 import NetflixNavigationLink from "../NetflixNavigationLink";
 
-const pages = ["My List", "Movies", "Tv Shows"];
+const pages = ["My New List", "All Movies", "All Tv Shows"];
 
 const MainHeader = () => {
   const isOffset = useOffSetTop(APP_BAR_HEIGHT);
