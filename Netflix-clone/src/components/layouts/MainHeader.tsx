afa4cc05d8dd0421c1e32f3804108d9460fbb15f
Hafsa ASSILA <101941769+C-team-project@users.noreply.github.com>
2025-01-01 10:45:33 +0100

    Update MainHeader.tsx

--- Netflix-clone/src/components/layouts/MainHeader.tsx
+++ Netflix-clone/src/components/layouts/MainHeader.tsx
@@ -111,7 +111,7 @@ const MainHeader = () => {
             textDecoration: "none",
           }}
         >
-          Netflix
+          Nepflix
         </Typography>
         <Stack
           direction="row"
